4f7d0608208157b07246c899592e9fcc7df98025
UgnePleseviciute <145859423+UgnePleseviciute@users.noreply.github.com>
2023-11-09 16:57:58 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,34 +1,13 @@
-#include "Studentas.h"
+#include "Stu.h"
+#include "Faprasai.h"
+
+
+
 int main() {
-    vector<Studentas> studentai;
-    char SkaiciavimoBudas;
-    char PridetiDarViena;
 
-    cout << "Pasirinkite skaiciavimo metoda (V- Vidurkis, M - Mediana): ";
-    cin >> SkaiciavimoBudas;
+    vector  <Studentas> studentai; // Studentų sąrašas
 
-    if (SkaiciavimoBudas != 'V' && SkaiciavimoBudas != 'v' && SkaiciavimoBudas != 'M' && SkaiciavimoBudas != 'm') {
-        cout << "Nesuprantama ivesti, naudosime Vidurki pagal Default." << endl;
-        SkaiciavimoBudas = 'V'; // Default to average
-    }
-
-    do {
-        Studentas studentas;
-        studentas.SkaiciavimoBudas = SkaiciavimoBudas;
-
-        StudentoDuomenys(studentas);
-        studentai.push_back(studentas);
-
-        cout << "Ar nortite prideti dar viena studenta? (T - Taip, N - Ne) ";
-        cin >> PridetiDarViena;
-    } while (PridetiDarViena == 'T' || PridetiDarViena == 't');
-
-    cout << "Studentu duomenys ir galutiniai balai:" << endl;
-    bool isHeader = true;
-    for (const Studentas& student : studentai) {
-        Isvedimas(student, isHeader);
-        isHeader = false;
-    }
+    PasirinktiVeiksma(studentai); // Kviečiama funkcija, kurioje vyksta pasirinkimas
 
     return 0;
 }
