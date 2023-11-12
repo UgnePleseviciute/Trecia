2c868da2bd5c9fb9b197fc0e4f22d77b625491f3
UgnePleseviciute <145859423+UgnePleseviciute@users.noreply.github.com>
2023-11-12 22:08:49 +0200

    CMake

---
+++ V0.1/CMakelists/RUN.bat
@@ -0,0 +1,5 @@
+cmake CMakelists.txt
+cmake --build .
+cmake --install .
+build\Debug\my_executable.exe
+pause
---
+++ V0.1/CMakelists/main.cpp
@@ -0,0 +1,12 @@
+#include "Stu.h"
+#include "FunkcijuAprasai.h"
+
+
+
+int main() {
+
+    vector  <Studentas> studentai;
+    PasirinktiVeiksma(studentai);
+
+    return 0;
+}
